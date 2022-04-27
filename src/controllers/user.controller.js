976192932edc89e397064202cdfb4ad7d3f37d9f
skale0181 <skale0181@gmail.com>
2022-04-27 21:17:02 +0530

    done with all user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,4 +4,29 @@ const User = require("../models/user.model");
 
 const router = express.Router();
 
+router.get("", async (req, res) => {
+    try {
+      const user = await User.find()
+        .lean()
+        .exec();
+      console.log(user);
+      return res.send(user);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+  });
+  
+router.get("/:name", async (req, res) => {
+    try {
+      // console.log(req.params.id);
+      const user = await User.find({ name: { $eq: req.params.name } })
+        .lean()
+        .exec();
+      console.log(user);
+      return res.send(user);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+  });
+
 module.exports = router;
